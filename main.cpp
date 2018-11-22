032205d915cd37c3dfd4c2787f69e2e528f6cec3
sikroz <sikroz@ispsystem.com>
2018-11-22 12:14:05 +0800

    Почти готово

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <Poco/URI.h>
+#include <nlohmann/json.hpp>
 
 using namespace std;
 
 int main() {
+	std::string uri_string;
+	std::cin >> uri_string;
+	nlohmann::json result;
+	result["uri"] = uri_string;
+	std::cout << result << std::endl;
 	return 0;
 }
